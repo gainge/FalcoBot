c2e1ef9e61aaa7fc55f5aa3e8aaaeb3670df628a
Gibson Ainge <gainge@gmail.com>
2020-07-13 14:14:31 -0600

    make compliment command more robust

--- commands/compliment.js
+++ commands/compliment.js
@@ -1,7 +1,7 @@
 const phrases = [
   'is super cool!',
   'has great fashion sense',
-  'has a great playstyle',
+  'has a sick playstyle',
   'has the best combos',
   'is always really nice!',
 ];
@@ -15,6 +15,10 @@ module.exports = {
   description: 'Say something nice about a user :)',
   guildOnly: true,
 	execute(message, args) {
+    if (!args.length) {
+      return message.channel.send(`${message.author} ${randomCompliment()}`); 
+    }
+
     if (!message.mentions.users) {
       message.channel.send('Please tag somebody to compliment');
     } else {
