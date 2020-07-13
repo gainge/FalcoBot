c01addd5f727a5d9d5b0248dd0e29a089c79ea17
Gibson Ainge <gainge@gmail.com>
2020-07-13 12:37:38 -0600

    add a silly test command

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
+client.on('message', message => {
+  console.log(`Falco Hears: ${message.content}`);
+
+  if (message.content.toLowerCase() === 'jv') {
+    message.channel.send('you won\'t');
+  }
+});
+
+
 client.login(`${process.env.DISCORD_BOT_TOKEN}`);
